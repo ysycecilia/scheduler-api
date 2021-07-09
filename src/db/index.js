7486b698911fa6ff2212f148e5c0c8424d232397
ysycecilia <ysycecilia@gmail.com>
2021-07-08 21:35:20 -0400

    connect to db

--- src/db/index.js
+++ src/db/index.js
@@ -5,6 +5,11 @@ const client = new pg.Client({
   ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false
 });
 
+// const client = new pg.Client({
+//   connectionString: process.env.DATABASE_URL || "",
+//   ssl: process.env.DATABASE_URL ? {rejectUnauthorized: false } : false
+// });
+
 client
   .connect()
   .catch(e => console.log(`Error connecting to Postgres server:\n${e}`));
